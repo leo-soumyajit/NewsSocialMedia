ee102061201b237cc7672f18f3813ea49bcef944
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-02-26 03:49:49 +0530

    Profiles added final production setup

--- src/main/java/com/soumyajit/news/social/media/app/NewsSocialMediaAppApplication.java
+++ src/main/java/com/soumyajit/news/social/media/app/NewsSocialMediaAppApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "News Social Media : ", version = "1.0", description = "The Social Media Platform API provides a comprehensive set of endpoints to manage and interact with user-generated news content on a news social media platform. Designed with scalability and ease of use in mind, this API allows for the creation, retrieval, liking, and remove like of posts and also comment in a post , as well as user authentication and authorization to ensure secure access to the platform."))
+//@EnableJpaRepositories("com.soumyajit.news.social.media.app.Repository")
 public class NewsSocialMediaAppApplication {
 
 	public static void main(String[] args) {
