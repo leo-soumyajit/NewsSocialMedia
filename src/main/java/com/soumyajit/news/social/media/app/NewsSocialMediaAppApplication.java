edef3b6ca9f1d2d6101e5d8e32b01f0ba11d840e
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-02-26 04:12:24 +0530

    jar file name reset

--- src/main/java/com/soumyajit/news/social/media/app/NewsSocialMediaAppApplication.java
+++ src/main/java/com/soumyajit/news/social/media/app/NewsSocialMediaAppApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "News Social Media : ", version = "1.0", description = "The Social Media Platform API provides a comprehensive set of endpoints to manage and interact with user-generated news content on a news social media platform. Designed with scalability and ease of use in mind, this API allows for the creation, retrieval, liking, and remove like of posts and also comment in a post , as well as user authentication and authorization to ensure secure access to the platform."))
-//@EnableJpaRepositories("com.soumyajit.news.social.media.app.Repository")
 public class NewsSocialMediaAppApplication {
 
 	public static void main(String[] args) {
